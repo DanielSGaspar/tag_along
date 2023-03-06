0b95b06a874fa40d98344aacafe215621c02b11e
Daniel Gaspar <daniel.s.gaspar@gmail.com>
2023-03-06 14:19:42 +0000

    Added routes for locations and beaches

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :locations, only: %i[index show] do
+    resources :beaches, only: %i[index show]
+  end
 end
