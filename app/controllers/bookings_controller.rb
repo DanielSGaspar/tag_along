9cdd5df917c93bc77f9056df01b3951840f538a0
Daniel Gaspar <daniel.s.gaspar@gmail.com>
2023-03-09 13:13:37 +0000

    Created new Booking page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,16 @@
 class BookingsController < ApplicationController
+  def new
+    @ride = Ride.find(params[:ride_id])
+    @booking = Booking.new
+  end
+
+  def create
+    raise
+  end
+
+  def show
+  end
+
+  def update
+  end
 end
