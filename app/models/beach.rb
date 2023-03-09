9d9b2a8f844458d20ca3b05db22c034470b7ac17
Sofia Eriksson <shadyseal@zohomail.eu>
2023-03-09 15:58:01 +0000

    improved seeds and store imgs in cloud

--- app/models/beach.rb
+++ app/models/beach.rb
@@ -1,4 +1,5 @@
 class Beach < ApplicationRecord
   belongs_to :location
   has_many :rides
+  has_one_attached :photo
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -3,4 +3,5 @@ class Location < ApplicationRecord
   NAMES = ["Minho & Douro", "Beira Litoral", "Peniche", "Ericeira", "Sintra", "Lisbon", "Costa da Caparica", "Alentejo & North Algarve", "South Algarve"]
   has_many :beaches, dependent: :destroy
   has_many :rides, through: :beaches
+  has_one_attached :photo
 end
