0092e00998b4e4256944d81f55dfa56d7508f681
Sofia Eriksson <shadyseal@zohomail.eu>
2023-03-14 23:28:03 +0000

    reviews profile and navbar

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :ride
   belongs_to :user
-  has_many :reviews, dependent: :destroy
   
+  has_many :reviews, dependent: :destroy
+
   validates :seats, presence: true
 end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,6 +3,8 @@ class Ride < ApplicationRecord
   belongs_to :user
   belongs_to :beach
   has_many :bookings
+  has_many :reviews, through: :bookings
+
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   delegate :location, to: :beach
