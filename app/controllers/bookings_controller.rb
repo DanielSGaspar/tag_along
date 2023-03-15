0d44b60548ebbd0d2f722547b51eda2e73151078
Daniel Gaspar <daniel.s.gaspar@gmail.com>
2023-03-15 14:47:26 +0000

    Fixed pyament/Checkout and did some minor front end changes

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -23,7 +23,7 @@ class BookingsController < ApplicationController
           quantity: 1,
           price_data: {
             currency: "eur",
-            unit_amount: @ride.price_cents,
+            unit_amount: @ride.price_cents * @booking.seats,
             product_data: {
               name: "Ride to #{@ride.beach.name}",
               images: ["https://images.unsplash.com/photo-1520116468816-95b69f847357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"],
@@ -37,7 +37,7 @@ class BookingsController < ApplicationController
       @booking.update(checkout_session_id: session.id)
       redirect_to new_booking_payment_path(@booking)
     else
-      render :show, status: :unprocessable_entity # Need to ask the TAs how to fix this
+      render "rides/show", status: :unprocessable_entity
     end
   end
 
