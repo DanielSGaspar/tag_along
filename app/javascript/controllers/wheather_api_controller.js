c15778f8980efc2ae0554da730c76111c9f9fa1c
Sofia Eriksson <shadyseal@zohomail.eu>
2023-03-16 10:39:21 +0000

    Changes to conditions on card

--- app/javascript/controllers/wheather_api_controller.js
+++ app/javascript/controllers/wheather_api_controller.js
@@ -13,6 +13,40 @@ export default class extends Controller {
     const lng = this.longValue
     const params = 'windSpeed,windDirection,waveHeight';
 
+    const translateWindDirection = (direction) => {
+      if ((direction > 337.5 && direction <= 360) || (direction > 0 && direction < 22.5)) {
+        return "N";
+      }
+
+      if (direction > 22.5 && direction <= 67.5) {
+        return "NE"
+      }
+
+      if (direction > 67.5 && direction <= 112.5) {
+        return "E"
+      }
+
+      if (direction > 112.5 && direction <= 157.5) {
+        return "SE"
+      }
+
+      if (direction > 157.5 && direction <= 202.5) {
+        return "S"
+      }
+
+      if (direction > 202.5 && direction <= 247.5) {
+        return "SW"
+      }
+
+      if (direction > 247.5 && direction <= 292.5) {
+        return "W"
+      }
+
+      if (direction > 292.5 && direction <= 337.5) {
+        return "NW"
+      }
+    }
+
     fetch(`https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${params}`, {
       headers: {
         'Authorization': this.apiKeyValue
@@ -21,7 +55,7 @@ export default class extends Controller {
       const windDirection = jsonData.hours[1].windDirection.noaa;
       const windSpeed = jsonData.hours[1].windSpeed.noaa;
       const waveHeight = jsonData.hours[1].waveHeight.noaa;
-      this.winddirectionTarget.innerHTML = windDirection
+      this.winddirectionTarget.innerHTML = translateWindDirection(windDirection)
       this.windspeedTarget.innerHTML = windSpeed
       this.waveheightTarget.innerHTML = waveHeight
     });
