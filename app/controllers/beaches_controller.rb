dad3fa6acf4b99fb563c42c2057246dfb7bbdffa
Daniel Gaspar <daniel.s.gaspar@gmail.com>
2023-03-16 22:15:46 +0000

    Fixed status update and favorite button

--- app/controllers/beaches_controller.rb
+++ app/controllers/beaches_controller.rb
@@ -1,14 +1,14 @@
 class BeachesController < ApplicationController
   def favorite
     @user = current_user
-    beach = Beach.find(params[:id])
+    @beach = Beach.find(params[:id])
 
-    if @user.favorited?(beach)
-      @user.unfavorite(beach)
+    if @user.favorited?(@beach)
+      @user.unfavorite(@beach)
     else
-      @user.favorite(beach)
+      @user.favorite(@beach)
     end
 
-    redirect_to beach_path(beach)
+    redirect_to beach_path(@beach, date: params[:date])
   end
 end
